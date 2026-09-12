0727e4e922fa638860557359f0aaed3bacfd5dc2
mrecap <testnameloneeddy@gmail.com>
2026-09-12 16:25:40 +0630

    Delete config.js

--- config.js
+++
@@ -1,2 +0,0 @@
-// Replace with your deployed Google Apps Script /exec URL.
-window.WANDEE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxo1xchDsVr6Zu1a1cUXYAFe1JJndkD9oDSjrVJv5j2GcBaXK911skt7HdCmeHPTNjPnA/exec';
