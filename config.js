1de16bfb7423abbe0bfab0e37844961245c263f3
mrecap <testnameloneeddy@gmail.com>
2026-09-12 15:17:10 +0630

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+// Paste your deployed Google Apps Script /exec URL here.
+window.WANDEE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxo1xchDsVr6Zu1a1cUXYAFe1JJndkD9oDSjrVJv5j2GcBaXK911skt7HdCmeHPTNjPnA/exec';
